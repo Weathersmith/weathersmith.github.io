007a01b8120fdc84e43b20fa6c5fcbd8bd0e418b
Samuel Smith <106135001+Weathersmith@users.noreply.github.com>
2022-05-25 22:31:03 -0600

    creating js file for sounding analysis

---
+++ sounding-analysis.js
@@ -0,0 +1,2 @@
+//This is a JS file that will contain all JS code regarding analyzing and isolating data from soundings. This will be used across the METOC web page
+//it is late at night when I am creating this and I am quite sleepy. Will start building this at a later time.
