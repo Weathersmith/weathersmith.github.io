f12e478f9143a2ef33dd1a3adec69e8c2feb4b71
Samuel Smith <106135001+Weathersmith@users.noreply.github.com>
2022-05-29 16:48:52 -0600

    changed out name of scripts, added logo and css file

--- sounding-analysis.js
+++
@@ -1,2 +0,0 @@
-//This is a JS file that will contain all JS code regarding analyzing and isolating data from soundings. This will be used across the METOC web page
-//it is late at night when I am creating this and I am quite sleepy. Will start building this at a later time.
