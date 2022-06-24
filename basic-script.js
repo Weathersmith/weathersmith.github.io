61b56100eca26f7b704e0a99936e4d965df01e31
Samuel Smith <106135001+Weathersmith@users.noreply.github.com>
2022-06-23 21:16:28 -0600

    sidenavs and more
    added sidenav functionality, testing color schemes, added blog page

--- basic-script.js
+++ basic-script.js
@@ -1,13 +1,11 @@
-function myDropdown() {
-  document.getElementById("myDropdown").classList.toggle("show");
+function openNav() {
+  document.getElementById("mySideNav").style.width = "250px";
+  document.getElementById("main").style.marginRight = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("myDropdown");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
-    }
-  }
+function closeNav() {
+  document.getElementById("mySideNav").style.width = "0";
+  document.getElementById("main").style.marginRight = "0";
+  document.body.style.backgroundColor = "whitesmoke";
 }
